a5d774a2bef43f91a916c9700a92a6d1facd58bd
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-12-05 23:49:27 +0200

    added and initialized bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
 
 # initializing the application
 app = Flask(__name__)
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
 
