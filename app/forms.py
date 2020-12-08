3eda5c0ae667b6115940f77540860ffcdcda3674
Ngabire Olivia Missy <ngamissia@gmail.com>
2020-12-08 03:28:22 +0200

    imported User model in forms

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 import email_validator
-
+from app import User
 
 class RegForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
